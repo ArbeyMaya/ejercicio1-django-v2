b815b98ae52f9d8bdabf4536a73803fd84ab53e8
Arbey <vilmarbey@gmail.com>
2024-09-16 21:57:21 -0500

    se conecto el css

--- medidor/urls.py
+++ medidor/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('contador/', views.contador_view, name='contador'),
-    path('ver-datos/', views.ver_datos_agua, name='ver_datos'),
     path('consumo/', views.promedio_consumo_view, name='promedio_consumo_view'),  # Nueva URL
-    path('establecer-limite/', views.establecer_límite_view, name='establecer_límite_view'),
-
+    path('limite/', views.establecer_límite_view, name='establecer_límite_view'),
+    
 ]
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sensor',
         'USER': 'root',
-        'PASSWORD': '1006997534',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
